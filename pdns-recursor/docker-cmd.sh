e615b8318c94765a78fc182d1d2fe1f568566e68
Peter Schiffer <peter@pschiffer.eu>
2018-11-06 22:51:45 +0100

    Update / fix pdns-recursor image

--- pdns-recursor/docker-cmd.sh
+++ pdns-recursor/docker-cmd.sh
@@ -5,19 +5,22 @@ set -euo pipefail
 # Configure base vars
 : "${PDNS_local_port:=53}"
 : "${PDNS_local_address:=0.0.0.0}"
+: "${PDNS_allow_from:=0.0.0.0/0}"
 
-export PDNS_local_port PDNS_local_address
+export PDNS_local_port PDNS_local_address PDNS_allow_from
+
+if [ -f /etc/fedora-release ]; then
+  config_file=/etc/pdns-recursor/recursor.conf
+  pdns_user=pdns-recursor
+elif [ -f /etc/alpine-release ]; then
+  config_file=/etc/pdns/recursor.conf
+  pdns_user=recursor
+fi
 
 # Create config file from template
-envtpl < /recursor.conf.tpl > /etc/pdns/recursor.conf
+envtpl < /recursor.conf.tpl > $config_file
 
-# fix config right
-if getent passwd | grep -q '^pdns-recursor:'; then
-    # Fedora user
-    chown pdns-recursor:pdns-recursor /etc/pdns/recursor.conf
-else
-    # Alpine user
-    chown recursor:recursor /etc/pdns/recursor.conf
-fi
+# Fix config file ownership
+chown ${pdns_user}: $config_file
 
 exec /usr/sbin/pdns_recursor
