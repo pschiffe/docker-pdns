3ba3021b68238e4d924d7fc0d13a1b544b5d07ef
murphy83 <github@murphyslantech.de>
2024-05-06 17:13:13 +0200

    Added possibility to use an existing config file for recursor or auth… (#135)
    * Added possibility to use an existing config file for recursor or authorative mysql-powerdns-server, in case the configuration file is handed in from a volume or bind mount (in which it should be mounted as read only!).
    
    * ammended changes to PostgreSQL init to provide the same features as the MySQL one.
    
    * do not forget to init superslaves if needed.
    
    * typos and really do install config file by environment.
    
    ---------
    
    Co-authored-by: Kai Schlachter <kai.schlachter@bahag.com>

--- pdns-recursor/docker-entrypoint.sh
+++ pdns-recursor/docker-entrypoint.sh
@@ -2,12 +2,17 @@
 
 set -eu
 
-# Configure base vars
-: "${PDNS_local_port:=53}"
-: "${PDNS_local_address:=0.0.0.0}"
-: "${PDNS_allow_from:=0.0.0.0/0}"
+#### Function definitions
+function deriveConfigValuesFromEnvrionement {
+  # Configure base vars
+  : "${PDNS_local_port:=53}"
+  : "${PDNS_local_address:=0.0.0.0}"
+  : "${PDNS_allow_from:=0.0.0.0/0}"
+  
+  export PDNS_local_port PDNS_local_address PDNS_allow_from
+}
 
-export PDNS_local_port PDNS_local_address PDNS_allow_from
+### end of function definitions
 
 if [ -f /etc/fedora-release ]; then
   config_file=/etc/pdns-recursor/recursor.conf
@@ -17,10 +22,18 @@ elif [ -f /etc/alpine-release ]; then
   pdns_user=recursor
 fi
 
+if [ ${USE_EXISTING_CONFIG_FILE:-false} = 'false' ]; then 
+    deriveConfigValuesFromEnvrionement
+    echo "generating config file from environment"
+    subvars --prefix 'PDNS_' < '/recursor.conf.tpl' > "${config_file}"
+    chown "${pdns_user}:" "${config_file}"
+  else
+    echo "using existing config file ${config_file}"
+fi    
+
 # Create config file from template
-subvars --prefix 'PDNS_' < '/recursor.conf.tpl' > "${config_file}"
 
 # Fix config file ownership
-chown "${pdns_user}:" "${config_file}"
+
 
 exec "$@"
